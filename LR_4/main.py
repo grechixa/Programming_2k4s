be47538eaecf0d2bce26454280b68523c1d34fc7
grechixa <grikurylev@gmail.com>
2026-03-30 15:02:22 +0300

    add 4 subsystems

--- LR_4/main.py
+++ LR_4/main.py
@@ -1,1 +1,25 @@
-# Hello
+class MusicSystem:
+    def __init__(self, genre: str, level: int, minutes: str):
+        pass
+    def play_music(self, genre):
+        pass
+    def set_volume(self, level):
+        pass
+    def set_timer(self, minutes):
+        pass
+
+class LightSystem:
+    def __init__(self, status: str):
+        pass
+    def set_status(self, status):
+        pass
+
+class TemperatureSystem:
+    def __init__(self, status: str, temp: int):
+        pass
+    def set_temp(self):
+        pass
+
+class AlarmSystem:
+    def __init__(self, at_time:str):
+        pass
